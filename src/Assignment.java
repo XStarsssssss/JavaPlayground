ef7fee5812c8ca1e6495b0860d7243e93704bffb
XStarsssssss <shinehtetaung30507@gmail.com>
2025-07-31 22:04:57 +0630

    update

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Assignment {
+    private double x;
+    private double y;
+    private double result;
+
+    public void  x(double x,double y){
+        this.x=x;
+        this.y=y;
+
+    }
+    public double  calculator(){
+
+        //return result = (x*x)+(5*x*x)-(3*x*x)-10-(3*x*y)-2-(10*y*x)+(y*y)-(x*x);
+         return result = Math.pow(x,2)+(5*Math.pow(x,2))-(3*Math.pow(x,2))-10-(3*x*y)-2-(10*y*x)+Math.pow(y,2)-Math.pow(x,2);
+    }
+    public void check(){
+        if (x < 0){
+            System.out.println("x is lower 0");
+        }
+        else if(y < 0){
+            System.out.println("y is lower 0");
+        }
+        else{
+            System.out.println("x: "+x);
+            System.out.println("y: "+y);
+            System.out.println("Result: "+calculator());
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ public class Main{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double bases = scanner.nextDouble();
-        double hight = scanner.nextDouble();
-        Formula formula = new Formula();
-        formula.area(bases,hight);
-        formula.check();
+//        double bases = scanner.nextDouble();
+//        double hight = scanner.nextDouble();
+//        Formula formula = new Formula();
+//        formula.area(bases,hight);
+//        formula.check();
+
+
+        Assignment assignment = new Assignment();
+        System.out.print("Enter x of value: ");
+        double x=scanner.nextInt();
+        System.out.print("Enter y of value: ");
+        double y = scanner.nextInt();
+        assignment.x(x,y);
+        assignment.check();
     }
 }
